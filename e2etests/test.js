0b550eb3ab1eb6e0f92b853a5ec88511e54f2de1
Bart <kluczynskib@gmail.com>
2017-10-05 13:16:27 +0100

    added selenium webdriver for e2e and changed the title to Memory Game

---
+++ e2etests/test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('MemoryGame App', () => {
+  it('should load with the right title', () => {
+    browser.url('http://localhost:3000/');
+    const actualTitle = browser.getTitle();
+
+    expect(actualTitle).to.eql('Memory Game');
+  })
+})
